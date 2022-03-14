3b9729ec9e453f0d0f42fe9c05f23047e22f99c3
Mokiy <92001109+MokiyCodes@users.noreply.github.com>
2022-03-14 21:54:48 +0100

    chore: Add Download Detection Stuff

--- ACRN.user.js
+++ ACRN.user.js
@@ -9,10 +9,12 @@
 // @source       https://github.com/MokiyCodes/GHUS/blob/main/ACRN.user.js
 // @author       MokiyCodes
 // @match        https://github.com/*
+// @match        https://mokiycodes.github.io/GHUS/*
 // @icon         https://img.mokiy.cc/cached/png?url=https://github.githubassets.com/favicons/favicon-dark.svg&width=4096&height=4096&density=8192
 // @grant        none
 // @run-at       document-body
 // @antifeature  loop We run a constant loop for each GitHub tab.
 // ==/UserScript==
 
+if (document.location.href.startsWith('https://mokiycodes.github.io/GHUS/download')) document.body.setAttribute('plugin-ACRN',true)
 setInterval(()=>{'use strict';document.querySelectorAll('.btn-danger.btn-block[type="submit"][disabled]').forEach(v=>v.removeAttribute('disabled'));document.querySelectorAll('input[aria-label^="Type in the name of the"]').forEach(v=>v.value=document.querySelector('[role="dialog"] .Box-footer p strong').innerText)document.querySelectorAll('details-dialog.Box.Box--overlay').forEach(v=>v.style.overflow="hidden")},1000);
