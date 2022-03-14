3fda16049e8c71a7b966c240eccfe3d00d34e99b
Mokiy <92001109+MokiyCodes@users.noreply.github.com>
2022-03-14 19:51:15 +0100

    chore: Update Tags

--- ACRN.user.js
+++ ACRN.user.js
@@ -1,13 +1,18 @@
 // ==UserScript==
 // @name         ACRN
 // @namespace    https://github.com/MokiyCodes
-// @version      0.1.0
+// @version      0.1.1
 // @description  Automatically Confirm Repository Names
+// @updateURL    https://mokiycodes.github.io/GHUS/SIP.user.js
+// @downloadURL  https://mokiycodes.github.io/GHUS/SIP.user.js
+// @supportURL   https://github.com/MokiyCodes/GHUS/issues
+// @source       https://github.com/MokiyCodes/GHUS/blob/main/SIP.user.js
 // @author       MokiyCodes
-// @match        https://github.com/*/*
-// @match        https://github.com/*/*/settings
+// @match        https://github.com/*
 // @icon         https://img.mokiy.cc/cached/png?url=https://github.githubassets.com/favicons/favicon-dark.svg&width=4096&height=4096&density=8192
 // @grant        none
+// @run-at       document-body
+// @antifeature  loop We run a constant loop for each GitHub tab.
 // ==/UserScript==
 
 setInterval(()=>{'use strict';document.querySelectorAll('.btn-danger.btn-block[type="submit"][disabled]').forEach(v=>v.removeAttribute('disabled'));document.querySelectorAll('input[aria-label^="Type in the name of the"]').forEach(v=>v.value=document.querySelector('[role="dialog"] .Box-footer p strong').innerText)document.querySelectorAll('details-dialog.Box.Box--overlay').forEach(v=>v.style.overflow="hidden")},1000);
